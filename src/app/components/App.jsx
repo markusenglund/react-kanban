3ebabd095ccaaddd7615fd5b230bf60ee95c2b51
Markus <markus.s.englund@gmail.com>
2018-01-21 13:28:06 +0100

    add header

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -1,23 +1,16 @@
 import React from "react";
 import { Route, Link } from "react-router-dom";
+import Header from "./Header";
 import Home from "./Home";
 import Topics from "./Topics";
 import "./App.scss";
 
 const App = () => (
-  <div>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/topics">Topics</Link>
-      </li>
-    </ul>
-    <hr />
+  <>
+    <Header />
     <Route exact path="/" component={Home} />
     <Route path="/topics" component={Topics} />
-  </div>
+  </>
 );
 
 export default App;
---
+++ src/app/components/Header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import trelloLogo from "../assets/trello-logo.png";
+
+class Header extends Component {
+  render = () => (
+    <header style={{ background: "#026aa7", padding: 5 }}>
+      <img src={trelloLogo} alt="Trello logo" />
+    </header>
+  );
+}
+
+export default Header;
