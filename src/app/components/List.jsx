e1e3b9e425c9e5ef1e99f54b6221be8288ec3080
Markus <markus.s.englund@gmail.com>
2018-01-21 17:25:49 +0100

    add flow

--- src/app/components/List.jsx
+++ src/app/components/List.jsx
@@ -1,6 +1,14 @@
+// @flow
 import React, { Component } from "react";
 
-class List extends Component {
+type Props = {
+  list: {
+    title: string,
+    cards: Array<{ title: string }>
+  }
+};
+
+class List extends Component<Props> {
   render = () => {
     const { list } = this.props;
     return (
