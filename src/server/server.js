db19035fc86dd982fd6403e2122db49658eec6af
Markus <markus.s.englund@gmail.com>
2018-01-27 20:03:32 +0100

    add dotenv for environment vars

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,20 @@
 import path from "path";
 import express from "express";
 import compression from "compression";
+import dotenv from "dotenv";
+import { Client } from "pg";
 import renderPage from "./renderPage";
+
+dotenv.config();
+
+const client = new Client();
+
+client.connect();
+
+client.query("SELECT $1::text as message", ["Hello world!"], (err, res) => {
+  console.log(err ? err.stack : res.rows[0].message); // Hello World!
+  client.end();
+});
 
 const app = express();
 
