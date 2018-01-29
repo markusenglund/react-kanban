a660b0fb74392820cec706bef8801caea0ce816a
Markus <markus.s.englund@gmail.com>
2018-01-29 21:55:39 +0100

    switch postgres to mysql

--- src/server/server.js
+++ src/server/server.js
@@ -3,35 +3,15 @@ import express from "express";
 import compression from "compression";
 import favicon from "serve-favicon";
 import dotenv from "dotenv";
-import { Client } from "pg";
 import renderPage from "./renderPage";
 
 dotenv.config();
-
-const getData = (req, res, next) => {
-  const client = new Client();
-
-  client.connect();
-
-  client.query("SELECT * FROM cards", (err, cardData) => {
-    // console.log(err ? err.stack : res.rows[0]); // Hello World!
-    console.log("getdata");
-    const cards = cardData.rows.reduce(
-      (acc, card) => ({ ...acc, [card.id]: card }),
-      {}
-    );
-    req.initialState = { cards };
-    client.end();
-    next();
-  });
-};
 
 const app = express();
 
 app.use(favicon(path.join("dist/public/favicons/favicon.ico")));
 app.use(compression());
 app.use("/public", express.static(path.join("dist/public")));
-app.use(getData);
 app.get("*", renderPage);
 
 const port = process.env.PORT || "1337";
