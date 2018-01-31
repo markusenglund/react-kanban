98397f1f0bee14ad9f71030139c7952ba3ba3296
Markus <markus.s.englund@gmail.com>
2018-01-31 18:12:29 +0100

    support empty boards and empty lists

--- src/server/server.js
+++ src/server/server.js
@@ -5,6 +5,7 @@ import compression from "compression";
 import favicon from "serve-favicon";
 import dotenv from "dotenv";
 import renderPage from "./renderPage";
+import reorganizeData from "./reorganizeData";
 
 dotenv.config();
 
@@ -16,67 +17,16 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
-const reorganizeData = data => {
-  /* eslint-disable camelcase */
-  const boardsById = data.reduce(
-    (boards, { board_id, board_title, list_id }) => {
-      if (!boards[board_id]) {
-        return {
-          ...boards,
-          [board_id]: { id: board_id, title: board_title, lists: [list_id] }
-        };
-      }
-      if (!boards[board_id].lists.includes(list_id)) {
-        return {
-          ...boards,
-          [board_id]: {
-            ...boards[board_id],
-            lists: [...boards[board_id].lists, list_id]
-          }
-        };
-      }
-      return boards;
-    },
-    {}
-  );
-  const listsById = data.reduce((lists, { list_id, list_title, card_id }) => {
-    if (!lists[list_id]) {
-      return {
-        ...lists,
-        [list_id]: { id: list_id, title: list_title, cards: [card_id] }
-      };
-    }
-    if (!lists[list_id].cards.includes(card_id)) {
-      return {
-        ...lists,
-        [list_id]: {
-          ...lists[list_id],
-          cards: [...lists[list_id].cards, card_id]
-        }
-      };
-    }
-    return lists;
-  }, {});
-  const cardsById = data.reduce(
-    (cards, { card_id, card_title }) => ({
-      ...cards,
-      [card_id]: { id: card_id, title: card_title }
-    }),
-    {}
-  );
-
-  return { boardsById, listsById, cardsById };
-  /* eslint-enable camelcase */
-};
-
 const getData = (req, res, next) => {
   connection.query(
     `SELECT boards.board_id, board_title, lists.list_id, list_title, card_id, card_title
-    FROM boards, lists, cards
-    WHERE cards.list_id = lists.list_id
-    AND lists.board_id = boards.board_id 
-    AND boards.is_public = 1
-    ORDER BY list_order, card_order`,
+    FROM boards
+    LEFT JOIN lists
+      ON lists.board_id = boards.board_id
+    LEFT JOIN cards
+      ON cards.list_id = lists.list_id
+    WHERE is_public = 1
+    ORDER BY list_order, card_order;`,
     (error, cards) => {
       if (error) console.log(error);
       req.initialState = reorganizeData(cards);
