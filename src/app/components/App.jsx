c1d5276557c85dfbfe0ad8c0fb9b5f45431e4238
Markus <markus.s.englund@gmail.com>
2018-02-09 22:37:49 +0100

    implement split based on state.user

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -1,19 +1,27 @@
 // @flow
 import * as React from "react";
 import { Route } from "react-router-dom";
+import { connect } from "react-redux";
 import Header from "./Header";
 import Home from "./Home";
 import Board from "./Board/Board";
 import LandingPage from "./LandingPage";
 import "./App.scss";
 
-const App = () => (
-  <>
-    <Header />
-    <Route exact path="/" component={Home} />
-    <Route exact path="/login" component={LandingPage} />
-    <Route path="/b/:boardId" component={Board} />
-  </>
-);
+const App = ({ user }) => {
+  if (user) {
+    return (
+      <>
+        <Header />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={LandingPage} />
+        <Route path="/b/:boardId" component={Board} />
+      </>
+    );
+  }
+  return <LandingPage />;
+};
 
-export default App;
+const mapStateToProps = state => ({ user: state.user });
+
+export default connect(mapStateToProps)(App);
