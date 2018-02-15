01b2d488e7675ea1c82380c946095a829a247fc6
Markus <markus.s.englund@gmail.com>
2018-02-15 13:24:28 +0100

    make dropdown menu button

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,16 +1,49 @@
+// @flow
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { Button, Wrapper, Menu, MenuItem } from "react-aria-menubutton";
 import trelloLogo from "../../assets/images/trello-logo.png";
+import "./Header.scss";
 
-class Header extends Component {
-  render = () => (
-    <header>
-      <Link to="/">
-        <img src={trelloLogo} alt="Trello logo" />
-      </Link>
-      <a href="/auth/signout">Sign out</a>
-    </header>
-  );
+type Props = {
+  user: {
+    name: string,
+    imageUrl: string
+  }
+};
+
+class Header extends Component<Props> {
+  render = () => {
+    const { name, imageUrl } = this.props.user;
+    return (
+      <header>
+        <Link to="/">
+          <img src={trelloLogo} alt="Trello logo" className="profile-image" />
+        </Link>
+        <Wrapper className="dropdown-wrapper">
+          <Button className="dropdown-button">
+            <img src={imageUrl} alt={name} />
+            &#9662;
+          </Button>
+          <Menu className="dropdown-menu">
+            {name}
+            <br />
+            <MenuItem
+              tag="a"
+              rel="noopener noreferrer"
+              href="/auth/signout"
+              className="dropdown-item"
+            >
+              Sign out
+            </MenuItem>
+          </Menu>
+        </Wrapper>
+      </header>
+    );
+  };
 }
 
-export default Header;
+const mapStateToProps = ({ user }) => ({ user });
+
+export default connect(mapStateToProps)(Header);
