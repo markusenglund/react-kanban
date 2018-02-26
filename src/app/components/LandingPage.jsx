6b710a0eb52f9fb8111ee817b4958ef59632ef1c
Markus <markus.s.englund@gmail.com>
2018-02-26 17:31:45 +0100

    add 'enter as guest' button to landing page

--- src/app/components/LandingPage.jsx
+++ src/app/components/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Helmet } from "react-helmet";
 import FaTwitter from "react-icons/lib/fa/twitter";
+import FaUserSecret from "react-icons/lib/fa/user-secret";
 import googleLogo from "../../assets/images/google-logo.svg";
 import kanbanLogo from "../../assets/images/kanban-logo.svg";
 import "./LandingPage.scss";
@@ -18,13 +19,18 @@ class LandingPage extends Component {
           <h1>kanban.live</h1>
         </div>
         <h2>An open source kanban application built with React and Redux</h2>
-        <a href="/auth/twitter" className="signin-button twitter-button">
-          <FaTwitter className="logo-icon" /> &nbsp;Sign in with Twitter
-        </a>
-        <a href="/auth/google" className="signin-button google-button">
-          <img className="google-logo" src={googleLogo} alt="google logo" />
-          &nbsp;Sign in with Google
-        </a>
+        <div className="signin-buttons">
+          <a href="/auth/twitter" className="signin-button twitter-button">
+            <FaTwitter className="logo-icon" /> &nbsp;Sign in with Twitter
+          </a>
+          <a href="/auth/google" className="signin-button google-button">
+            <img className="google-logo" src={googleLogo} alt="google logo" />
+            &nbsp;Sign in with Google
+          </a>
+          <a href="/?guest=true" className="signin-button guest-button">
+            <FaUserSecret className="logo-icon" /> &nbsp;Enter as guest
+          </a>
+        </div>
       </div>
     </div>
   );
