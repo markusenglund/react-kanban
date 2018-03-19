e8f6b4f5a6a44d30e4a2bb1fbbd97f68da07b86e
Markus <markus.s.englund@gmail.com>
2018-03-19 20:45:44 +0100

    replace header-dropdown with a neat button and image

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,19 +1,51 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import UserDropdown from "./UserDropdown";
+import FaUserSecret from "react-icons/lib/fa/user-secret";
+import FaSignOut from "react-icons/lib/fa/sign-out";
+import FaSignIn from "react-icons/lib/fa/sign-in";
 import kanbanLogo from "../../../assets/images/kanban-logo.svg";
 import "./Header.scss";
 
 class Header extends Component {
-  render = () => (
-    <header>
-      <Link to="/" className="header-title">
-        <img src={kanbanLogo} alt="kanban live logo" />
-        &nbsp;kanban.live
-      </Link>
-      <UserDropdown />
-    </header>
-  );
+  static propTypes = { user: PropTypes.object };
+  render = () => {
+    const { user } = this.props;
+    return (
+      <header>
+        <Link to="/" className="header-title">
+          <img src={kanbanLogo} alt="kanban live logo" />
+          &nbsp;kanban.live
+        </Link>
+        <div className="header-right-side">
+          {user ? (
+            <img
+              src={user.imageUrl}
+              alt={user.name}
+              className="user-thumbnail"
+              title={user.name}
+            />
+          ) : (
+            <FaUserSecret style={{ fontSize: 24, paddingTop: 2 }} />
+          )}
+          {user ? (
+            <a className="signout-link" href="/auth/signout">
+              <FaSignOut className="signout-icon" />
+              &nbsp;Sign out
+            </a>
+          ) : (
+            <a className="signout-link" href="/">
+              <FaSignIn className="signout-icon" />
+              &nbsp;Sign in
+            </a>
+          )}
+        </div>
+      </header>
+    );
+  };
 }
 
-export default Header;
+const mapStateToProps = ({ user }) => ({ user });
+
+export default connect(mapStateToProps)(Header);
