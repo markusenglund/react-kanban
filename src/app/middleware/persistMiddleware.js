b5bc891de1e7f3c316735907ad6cded8472ff30c
Markus <markus.s.englund@gmail.com>
2018-03-21 23:45:16 +0100

    cleanup client and middleware

--- src/app/middleware/persistMiddleware.js
+++ src/app/middleware/persistMiddleware.js
@@ -1,9 +1,12 @@
 import { denormalize, schema } from "normalizr";
 import axios from "axios";
 
+// Persist the board to the database after every action.
 const persistMiddleware = store => next => action => {
   next(action);
   const { user, boardsById, listsById, cardsById } = store.getState();
+
+  // Nothing is persisted for guest users
   if (user) {
     const { boardId } = action.payload;
 
@@ -11,7 +14,11 @@ const persistMiddleware = store => next => action => {
       axios
         .delete("/api/board", { data: { boardId } })
         .then(({ data }) => console.log(data));
+
+      // All action-types that are not DELETE_BOARD are currently modifying a board in a way that should
+      // be persisted to db. If other types of actions are added, this logic has be get more specific.
     } else {
+      // Transform the flattened board state structure into the tree-shaped structure that the db uses.
       const card = new schema.Entity("cardsById", {}, { idAttribute: "_id" });
       const list = new schema.Entity(
         "listsById",
@@ -26,6 +33,7 @@ const persistMiddleware = store => next => action => {
       const entities = { cardsById, listsById, boardsById };
 
       const boardData = denormalize(boardId, board, entities);
+
       axios.put("/api/board", boardData).then(({ data }) => console.log(data));
     }
   }
--- src/client.jsx
+++ src/client.jsx
@@ -6,31 +6,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { BrowserRouter } from "react-router-dom";
 import reducers from "./app/reducers/reducers";
-import persistMiddleware from "./persistMiddleware";
+import persistMiddleware from "./app/middleware/persistMiddleware";
 import App from "./app/components/App";
 
+// Hydrate redux state received from the server
 const preloadedState = window.PRELOADED_STATE;
-
 delete window.PRELOADED_STATE;
 
 const store = createStore(
   combineReducers(reducers),
   preloadedState,
-  composeWithDevTools(applyMiddleware(thunk, persistMiddleware))
+  composeWithDevTools(applyMiddleware(persistMiddleware))
 );
-const render = () => {
-  ReactDOM.hydrate(
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>,
-    document.getElementById("app")
-  );
-};
 
-render();
+ReactDOM.hydrate(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("app")
+);
