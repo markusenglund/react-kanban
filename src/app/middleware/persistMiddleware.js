fe939812987f686957fdb6fad565460d1f41f5c4
Markus <markus.s.englund@gmail.com>
2018-03-22 12:46:10 +0100

    remove console.logs after write operations

--- src/app/middleware/persistMiddleware.js
+++ src/app/middleware/persistMiddleware.js
@@ -15,12 +15,10 @@ const persistMiddleware = store => next => action => {
   // Nothing is persisted for guest users
   if (user) {
     if (action.type === "DELETE_BOARD") {
-      axios
-        .delete("/api/board", { data: { boardId } })
-        .then(({ data }) => console.log(data));
+      axios.delete("/api/board", { data: { boardId } });
 
-      // All action-types that are not DELETE_BOARD are currently modifying a board in a way that should
-      // be persisted to db. If other types of actions are added, this logic has be get more specific.
+      // All action-types that are not DELETE_BOARD or PUT_BOARD_ID_IN_REDUX are currently modifying a board in a way that should
+      // be persisted to db. If other types of actions are added, this logic will get unwieldy.
     } else if (action.type !== "PUT_BOARD_ID_IN_REDUX") {
       // Transform the flattened board state structure into the tree-shaped structure that the db uses.
       const card = new schema.Entity("cardsById", {}, { idAttribute: "_id" });
@@ -38,7 +36,7 @@ const persistMiddleware = store => next => action => {
 
       const boardData = denormalize(boardId, board, entities);
 
-      axios.put("/api/board", boardData).then(({ data }) => console.log(data));
+      axios.put("/api/board", boardData);
     }
   }
 };
