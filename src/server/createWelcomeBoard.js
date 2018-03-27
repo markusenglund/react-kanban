8b419d6056649d947a570d18b3e8c04aa27ac850
Markus <markus.s.englund@gmail.com>
2018-03-27 21:47:47 +0200

    improve welcome board

--- src/server/createWelcomeBoard.js
+++ src/server/createWelcomeBoard.js
@@ -11,17 +11,18 @@ const appendAttributes = list =>
 // Generate the initial showcase board that every user and guest gets when they first log in
 const createWelcomeBoard = userId => {
   const list1 = [
+    { text: "### An open source kanban application inspired by Trello" },
     {
       text: `![Octocat](https://assets-cdn.github.com/images/modules/logos_page/Octocat.png)
 Check out the [source code on GitHub](https://github.com/yogaboll/react-kanban)
 `,
       color: "#6df"
-    },
+    }
+  ];
+
+  const list2 = [
     {
-      text: "### Full support for touch screens and keyboard navigation."
-    },
-    {
-      text: `### Supports GitHub flavored markdown
+      text: `### GitHub flavored markdown
 Featuring cutting edge HTML features like
 * Headings
 * Bullet points
@@ -33,32 +34,41 @@ Featuring cutting edge HTML features like
     // Code blocks
 }
 \`\`\`
-* [x] Checkboxes
 
 Watch out, Netscape navigator 2.0!`
+    },
+    {
+      text: `### Works on mobile devices
+Unlike a certain other website...`
+    },
+    {
+      text: `### And more!
+[x] Colors
+[x] Deadlines
+[x] Checkboxes`,
+      color: "#ff6",
+      date: new Date()
     }
   ];
 
-  const list2 = [
+  const list3 = [
     {
-      text: `### Drag cards and lists
-Of course you can reposition both the cards and the lists by dragging them with a mouse or touch gesture, but you can also do it with just the keyboard. Try it out by focusing a card or list title, pressing Space bar and use the arrow keys to reposition the card or list`
+      text: `### Edit a card
+You can edit the contents of a card by clicking on it. Remember to use Shift + Enter to create a newline.`
+    },
+    {
+      text: `### Drag a card or list
+Reposition cards and lists by dragging them with a mouse or touch gesture.`
     },
     {
       text: `### Create a card or list
 Add a new card to an existing list by clicking the + button below each list. You can add a new list by clicking the "Add a list"-button to the right`
     },
     {
-      text: `### Edit a card
-You can edit the contents of a card by clicking on it or focusing it and pressing the Enter key. From this menu you can also give your card a color and a due date, or delete the card.`,
-      color: "#ff6",
-      date: new Date()
-    },
-    {
       text: `### Add a checklist
 For a task that has many sub-tasks, you can create a checklist with markdown.
-[ ] List this
-[x] And like this!`
+[x] Like this
+[ ] Click me`
     },
     {
       text: `### Change the board
@@ -66,9 +76,9 @@ You can edit the title of the board by clicking it. You can also change the colo
     }
   ];
 
-  // Append a warning message to the top of list2 for guest users only
+  // Append a warning message to the top of list3 for guest users only
   if (!userId) {
-    list2.unshift({
+    list3.unshift({
       text: `### Sign in to save changes
 Since you are not signed in, your changes will not persist after you leave the website. Go back to the login screen by pressing the 'Sign in' button in the top right corner.`
     });
@@ -76,18 +86,23 @@ Since you are not signed in, your changes will not persist after you leave the w
 
   return {
     _id: shortid.generate(),
-    title: "Welcome to React Kanban!",
+    title: "Tutorial board",
     color: "blue",
     lists: [
       {
         _id: shortid.generate(),
-        title: "A trello-like app built with React",
+        title: "Welcome to React Kanban!",
         cards: appendAttributes(list1)
       },
       {
         _id: shortid.generate(),
-        title: "How to use",
+        title: "Features",
         cards: appendAttributes(list2)
+      },
+      {
+        _id: shortid.generate(),
+        title: "How to use",
+        cards: appendAttributes(list3)
       }
     ],
     users: userId ? [userId] : []
