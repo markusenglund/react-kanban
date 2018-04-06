552110b6c2551fa59614eb8a6dacdb2e23cd2956
Markus <markus.s.englund@gmail.com>
2018-04-06 16:19:57 +0200

    comment out signin and db stuff from server.js so it just works

--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,7 @@ import session from "express-session";
 import connectMongo from "connect-mongo";
 import compression from "compression";
 import helmet from "helmet";
-import enforce from "express-sslify";
+// import enforce from "express-sslify";
 import favicon from "serve-favicon";
 import logger from "morgan";
 import dotenv from "dotenv";
@@ -22,39 +22,40 @@ const app = express();
 
 const MongoStore = connectMongo(session);
 
-MongoClient.connect(process.env.MONGODB_URL).then(client => {
-  const db = client.db(process.env.MONGODB_NAME);
+// MongoClient.connect(process.env.MONGODB_URL).then(client => {
+//   const db = client.db(process.env.MONGODB_NAME);
 
-  configurePassport(db);
+//   configurePassport(db);
 
-  // Uncomment next line for production to force https redirect
-  // app.use(enforce.HTTPS({ trustProtoHeader: true }));
-  app.use(helmet());
-  app.use(logger("tiny"));
-  app.use(compression());
-  app.use(favicon("dist/public/favicons/favicon.ico"));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  // aggressive cache static assets (1 year)
-  app.use("/static", express.static("dist/public", { maxAge: "1y" }));
+// Uncomment next line for production to force https redirect
+// app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(helmet());
+app.use(logger("tiny"));
+app.use(compression());
+app.use(favicon("dist/public/favicons/favicon.ico"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// aggressive cache static assets (1 year)
+app.use("/static", express.static("dist/public", { maxAge: "1y" }));
 
-  // Persist session in mongoDB
-  app.use(
-    session({
-      store: new MongoStore({ db }),
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false
-    })
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use("/auth", auth);
-  app.use("/api", api(db));
-  app.use(fetchBoardData(db));
-  app.get("*", renderPage);
+// Persist session in mongoDB
+// app.use(
+//   session({
+//     store: new MongoStore({ db }),
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false
+//   })
+// );
+app.use(passport.initialize());
+app.use(passport.session());
+// app.use("/auth", auth);
+// app.use("/api", api(db));
+// app.use(fetchBoardData(db));
+app.use(fetchBoardData());
+app.get("*", renderPage);
 
-  const port = process.env.PORT || "1337";
-  /* eslint-disable no-console */
-  app.listen(port, () => console.log(`Server listening on port ${port}`));
-});
+const port = process.env.PORT || "1337";
+/* eslint-disable no-console */
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+// });
