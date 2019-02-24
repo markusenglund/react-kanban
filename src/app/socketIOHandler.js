2ac8b99eadcaf1ce7d7abacb21fa4ebf0270b737
ShragaUser <dandankidon@gmail.com>
2019-02-24 19:36:53 +0200

    added location var

--- src/app/socketIOHandler.js
+++ src/app/socketIOHandler.js
@@ -1,5 +1,6 @@
 import socketIOClient from 'socket.io-client';
+const socketLocation = "localhost:8200/";
 
-const socket = socketIOClient("localhost:8200");
+const socket = socketIOClient(socketLocation);
 
 export default socket;
