0ffe24d0591d65071649d2855513743435c9574f
ShragaUser <dandankidon@gmail.com>
2019-02-26 13:18:36 +0200

    added socket location to constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 export const ADMIN_ROLE = 'admin';
 export const READ_WRITE_ROLE = 'read-write';
 export const READ_ROLE = 'read';
+export const SOCKETLOCATION = "localhost:8200/";
 
 export const DEFAULT_ROLE = READ_WRITE_ROLE;
