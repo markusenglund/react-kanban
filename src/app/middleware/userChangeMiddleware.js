7b1915e7a10b0e090aabb27de9451a86f42550d7
weizmm <weizmm05@gmail.com>
2019-02-27 19:41:27 +0200

    feat(push-notifications): adds saving every change on user
    also send "change" event in socket for every change on notifications.
    saves every change on different user in notifications collection

---
+++ src/app/middleware/userChangeMiddleware.js
@@ -0,0 +1,41 @@
+const userChangeMiddleware = store => next => action => {
+    next(action);
+    const {
+        user,
+        currentBoardId: boardId
+      } = store.getState();
+
+    if (user) {
+        if (['UPDATE_ASSIGNED_USER', 'ADD_USER','REMOVE_USER','CHANGE_USER_ROLE'].includes(action.type)){
+            switch (action.type){
+                case "UPDATE_ASSIGNED_USER": {
+                    let {assignedUserId} = action.payload;
+                    postWithParams(assignedUserId, boardId,action.type)
+                }
+                case 'ADD_USER': {
+                    let{userToAdd: user} = action.payload;
+                    postWithParams(user.id, boardId, action.payload);
+                }
+                case 'REMOVE_USER': {
+                    let{userIdToRemove: userId} = action.payload;
+                    postWithParams(userId, boardId, action.payload);
+                }
+                case 'CHANGE_USER_ROLE': {
+                    let{userId} = action.payload;
+                    postWithParams(userId, boardId, action.payload);
+                }
+            }
+        }   
+    }
+};
+
+function postWithParams(userId,boardId,action){
+    fetch("/api/notifications", {
+        method: "POST",
+        body: JSON.stringify({userId,boardId,action}),
+        headers: { "Content-Type": "application/json" },
+        credentials: "include"
+      })
+}
+
+export default userChangeMiddleware;
