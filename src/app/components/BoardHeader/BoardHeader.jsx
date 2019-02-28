795acda32c344bf974de9032a126a96466e60d42
Almog Vagman Ciprut <almogvc@gmail.com>
2019-02-28 20:06:29 +0200

    refactor: Remove add-user component

--- src/app/components/BoardHeader/BoardHeader.jsx
+++ src/app/components/BoardHeader/BoardHeader.jsx
@@ -2,18 +2,14 @@ import React from "react";
 import BoardTitle from "./BoardTitle";
 import ColorPicker from "./ColorPicker";
 import BoardDeleter from "./BoardDeleter";
-import BoardUserAdd from "./BoardUserAdder";
 import BoardLeave from "./BoardLeave";
 
-import {Popover,Pane,Text,Button} from 'evergreen-ui';
 import "./BoardHeader.scss";
 
 const BoardHeader = () => (
   <div className="board-header">
     <BoardTitle />
     <div className="board-header-right">
-      <BoardUserAdd />
-      <div className="vertical-line" />
       <ColorPicker />
       <div className="vertical-line" />
       <BoardDeleter />
