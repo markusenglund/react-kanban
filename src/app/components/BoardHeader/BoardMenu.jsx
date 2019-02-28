86a6dc73e13bb37ed0f71d8f93e118fd5fad171b
Almog Vagman Ciprut <almogvc@gmail.com>
2019-02-28 13:55:20 +0200

    fix: Fix board menu animations

--- src/app/components/BoardHeader/BoardMenu.jsx
+++ src/app/components/BoardHeader/BoardMenu.jsx
@@ -10,24 +10,16 @@ import { timingSafeEqual } from "crypto";
 
 class BoardMenu extends Component {
   state = {
-    open: true,
+    open: true
   };
 
   openSideBar = () => {
     this.setState({ open: true });
-    this.refs.sideNav.style.position = "absolute";
-    this.refs.sideNav.style.transition = "width 1s";
     this.refs.sideNav.style.width = "339px";
-    this.refs.sideNav.style.display = "block";
-   // document.getElementById("lists").style.marginLeft = "339px";
-    this.setState({ open: true });
   };
 
   closeSideBar = () => {
     this.setState({ open: false });
-    // document.getElementById("lists").style.marginLeft = "0px";
-    this.refs.sideNav.style.transition = "width 1s";
-    this.refs.sideNav.style.position = "absolute";
     this.refs.sideNav.style.width = "0px";
   };
 
@@ -35,45 +27,47 @@ class BoardMenu extends Component {
     return (
       <React.Fragment>
         <div
-          style={{
-            backgroundColor: "rgb(245, 246, 247)",
-            width: "339px",
-            transition: "all 1s ease 1s",
-            bottom: "0%",
-            position: "absolute",
-            borderTopRightRadius: "6px",
-            left: "0px",
-            top: "8%"
-          }}
-          ref="sideNav"
-        >
-          <div>
-            <h3 style={{ textAlign: "center", color: "black" }}>{this.props.t("Menu")}</h3>
-            {this.state.open ? (
-              <FaAngleLeft
-                className="hamburger-button"
-                style={{
-                  position: "absolute",
-                  right: "0%",
-                  top: "1.5%",
-                  color: "black"
-                }}
-                onClick={this.closeSideBar}
-              />
-            ) : null}
-          </div>
-          <hr
-            style={{
-              backgroundColor: "rgba(9,45,66,.13)",
-              border: 0,
-              height: "1px",
-              margin: "16px 0",
-              padding: 0,
-              width: "100%"
-            }}
-          />
-          <UsersList />
+        style={{
+          backgroundColor: "rgb(245, 246, 247)",
+          width: "339px",
+          transition: "1s",
+          bottom: "0%",
+          position: "fixed",
+          borderTopRightRadius: "6px",
+          left: "0px",
+          top: "8%"
+        }}
+        ref="sideNav"
+      >
+        <div>
+          <h3 style={{ textAlign: "center", color: "black" }}>
+            {this.props.t("Menu")}
+          </h3>
+          {this.state.open ? (
+            <FaAngleLeft
+              className="hamburger-button"
+              style={{
+                position: "absolute",
+                right: "0%",
+                top: "1.5%",
+                color: "black"
+              }}
+              onClick={this.closeSideBar}
+            />
+          ) : null}
         </div>
+        <hr
+          style={{
+            backgroundColor: "rgba(9,45,66,.13)",
+            border: 0,
+            height: "1px",
+            margin: "16px 0",
+            padding: 0,
+            width: "100%"
+          }}
+        />
+        <UsersList />
+      </div>
         {!this.state.open ? (
           <FaAngleRight
             className="hamburger-button"
